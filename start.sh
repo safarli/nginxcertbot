90a53a6abf3ae3522c1757711fbd2b6365b8319d
Bendali Safarli <ccsafarli@gmail.com>
2022-04-09 17:36:50 +0300

    script update

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 
-
+## RUN NGINX ONLY FOR CERTIFICATE ACQUIREMENT
 # running nginx service individually
 docker-compose run --service-ports nginx
+# OR
+docker-compose up -d nginx
 
+## ISSUE & RENEW CERTIFICATE
 # after ngi̇nx run, run the certbot as well
 docker-compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ -d example.org
-
 # run whenever certificate renewal needed
 docker compose run --rm certbot renew
